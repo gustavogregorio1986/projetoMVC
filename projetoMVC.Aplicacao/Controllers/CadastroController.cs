a52f35b316259c9011ab9b15357d0f97fcfb783f
guga56 <gustavogregorio086@gmail.com>
2022-07-02 22:03:30 -0300

    atualizando projeto

--- projetoMVC.Aplicacao/Controllers/CadastroController.cs
+++ projetoMVC.Aplicacao/Controllers/CadastroController.cs
@@ -15,61 +15,11 @@ namespace projetoMVC.Aplicacao.Controllers
         private IProdutoServico produtoServico;
         private IPagamentoServico pagamentoServico;
 
-        public void Cadastrar(ClienteView clienteVM, LojaView lojaVM, ProdutoView produtoVM, PagamentoView pagamentoVM)
-        {
-            try
-            {
-                clienteVM = new ClienteView();
-                Cliente cliente = new Cliente();
-                clienteVM.IdCliente = cliente.IdCliente;
-                clienteVM.NomeCliente = cliente.NomeCliente;
-                clienteVM.EmailCliente = cliente.EmailCliente; 
-
-                lojaVM = new LojaView();
-                Loja loja = new Loja();
-                lojaVM.IdLoja = loja.IdLoja;
-                lojaVM.NomeLoja = loja.NomeLoja;
-                
-
-                produtoVM = new ProdutoView();
-                Produto produto = new Produto();
-                produtoVM.IdProduto = produto.IdProduto;
-                produtoVM.NomeProd = produto.NomeProd;
-
-                pagamentoVM = new PagamentoView();
-                Pagamento pagamento = new Pagamento();
-                pagamentoVM.IdPagamento = pagamento.IdPagamento;
-                pagamentoVM.Preco = pagamento.Preco;
-                pagamentoVM.Quantidade = pagamento.Quantidade;
-
-                servicoCliene.AdiconarCliente(cliente);
-                lojaServico.AdiconarLoja(loja);
-                produtoServico.AdiconarPRoduto(produto);
-                pagamentoServico.AdiconarPagamento(pagamento);
-            }
-            catch
-            {
-                throw;
-            }
-        }
-
         public IActionResult Cadastrar()
         {
             return View();
         }
 
-
-
-        public List<Loja> ListarLojas()
-        {
-            var lista = new List<Loja>();
-
-            foreach(var loja in lista)
-            {
-                lista.Add(loja);
-            }
-
-            return lista;
-        }
+       
     }
 }
